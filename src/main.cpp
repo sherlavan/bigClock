7bdd5fb76bf2f9cb8633a35cd9a6f26d73927e75
sherlavan <sherlavan@gmail.com>
2023-09-22 22:35:02 +0300

    commands changed device change to esp32 wroom32

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,15 @@
 // #include "driver/uart.h"
 // #include "string.h"
 // #include "driver/gpio.h"
-// #include <inttypes.h>
+#include <inttypes.h>
 #include <HardwareSerial.h>
 #include "uartClockStationCommands.h"
 
 // static const int RX_BUF_SIZE = 1024;
 
 //! For ESP32 c3 13 UART0 RXPIN = 20, TXPIN = 21
-#define TXD_PIN (GPIO_NUM_5)
-#define RXD_PIN (GPIO_NUM_4)
+#define TXD_PIN (GPIO_NUM_22)//(GPIO_NUM_5)
+#define RXD_PIN (GPIO_NUM_23)//(GPIO_NUM_4)
 // #define UART (UART_NUM_1)
 // Led pins
 #define WHITE_LED (GPIO_NUM_19)
@@ -62,18 +62,9 @@
 static uint8_t sizeOfAnsver = 0;
 void setup() {
   // put your setup code here, to run once:
-  pinMode(WHITE_LED, OUTPUT);
-  pinMode(ORANGE_LED, OUTPUT);
-  pinMode(RED_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
-  pinMode(BLUE_LED, OUTPUT);
-  digitalWrite(BLUE_LED,0);
-  digitalWrite(GREEN_LED, 0);
-  digitalWrite(RED_LED, 0);
-  digitalWrite(WHITE_LED,0);
-  digitalWrite(ORANGE_LED, 0);
+
   Serial.begin(115200); // 
-  Serial1.setRxBufferSize(130);// must be > 128 f.e. 129
+  Serial1.setRxBufferSize(255);// must be > 128 f.e. 129
   Serial1.begin(57600, SERIAL_8N1, RXD_PIN, TXD_PIN); // uart1 with clock station
   // static byte commandCS[20]; // store computed command with check summ and end of packet
   
@@ -89,7 +80,7 @@ void loop() {
   
   Serial.print("program start\r\n");
 
-  unsigned char *  command = buildCMD(ReadParametrsCMD, ParametrsCMD);
+  unsigned char *  command = buildCMD(ReadModelCMD, ParametrsCMD);
 
   uint8_t commandLen = command[0];
   uint8_t answerLen = command[commandLen-1];
@@ -118,18 +109,22 @@ void loop() {
   Serial.println();
 
 
-  free(commandToSend);
+  
 
   
   // unsigned char *bytecmd;
   // bytecmd = 0;
   Serial1.write(commandToSend, commandLen - 2);
+  unsigned long responseTime = millis();
+
+  free(commandToSend);
   // delay(1000);
   
   unsigned char * answer = (unsigned char *) malloc(answerLen + 1);
-  for (u8_t i = 0;i<answerLen;i++){
+  for (uint8_t i = 0;i<answerLen;i++){
     answer[i]=0;
   }
+// @todo read string from bufer len = 255?
   while (Serial1.available()<answerLen)
   {
     delay(10);
@@ -139,13 +134,14 @@ void loop() {
     // sizeOfAnsver = Serial1.available();
     // Serial.println(sizeOfAnsver);
     // free answer);
-    
-    Serial1.readBytesUntil(0xfe, answer, answerLen);
+    Serial.print("Response time : ");
+    Serial1.readBytesUntil(0xfe, answer, answerLen + 1);
+    Serial.println(millis() - responseTime);
 
   }
 
   Serial.print("Recive response : ");
-    for(u8_t i = 0; i<answerLen; i++){
+    for(uint8_t i = 0; i<answerLen; i++){
       Serial.print(answer[i], HEX);
       Serial.print(" ");
       }
