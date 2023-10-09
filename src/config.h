5984f9876efcc4476581f71eaf5576622d6cc8a0
sherlavan <sherlavan@gmail.com>
2023-10-09 21:34:46 +0300

    test

--- src/config.h
+++ src/config.h
@@ -1,8 +1,8 @@
 #if !defined(__ESPCONFIG__)
 #define __ESPCONFIG__
 
-#define TXD_PIN_ClockStation (GPIO_NUM_02)//(GPIO_NUM_5) for esp32c3
-#define RXD_PIN_ClockStation (GPIO_NUM_04)//(GPIO_NUM_4) for esp32c3
+#define TXD_PIN_ClockStation (GPIO_NUM_2)//(GPIO_NUM_5) for esp32c3
+#define RXD_PIN_ClockStation (GPIO_NUM_4)//(GPIO_NUM_4) for esp32c3
 
 #define RXD_PIN_ClockMeh (GPIO_NUM_35)//UART for rs485 connections to clock mehanics
 #define TXD_PIN_ClockMeh (GPIO_NUM_34)//UART for rs485 connections to clock mehanics
@@ -12,5 +12,9 @@
 #define CMSerial Serial2//rename UART for ClockMehanics
 #define SIMSerial Serial0
 
+
+const char* host = "ClockStation";
+const char* ssid = "wifi";
+const char* Wpass = "F31wv32P";
 
 #endif // __ESPCONFIG__
--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,52 @@
 #include "uartClockStationCommands.h"
 #include "BluetoothSerial.h"
 #include "config.h"
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <Update.h>
+#include <ETH.h>
 
 
 BluetoothSerial SBT;
+WebServer server(80);
+
+const char* UpdatePage = 
+"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+   "<input type='file' name='update'>"
+        "<input type='submit' value='Update'>"
+    "</form>"
+ "<div id='prg'>progress: 0%</div>"
+ "<script>"
+  "$('form').submit(function(e){"
+  "e.preventDefault();"
+  "var form = $('#upload_form')[0];"
+  "var data = new FormData(form);"
+  " $.ajax({"
+  "url: '/update',"
+  "type: 'POST',"
+  "data: data,"
+  "contentType: false,"
+  "processData:false,"
+  "xhr: function() {"
+  "var xhr = new window.XMLHttpRequest();"
+  "xhr.upload.addEventListener('progress', function(evt) {"
+  "if (evt.lengthComputable) {"
+  "var per = evt.loaded / evt.total;"
+  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+  "}"
+  "}, false);"
+  "return xhr;"
+  "},"
+  "success:function(d, s) {"
+  "console.log('success!')" 
+ "},"
+ "error: function (a, b, c) {"
+ "}"
+ "});"
+ "});"
+ "</script>";
 
 
 
@@ -63,15 +106,51 @@ void setup() {
   CMSerial.begin(9600, SERIAL_8N1);// uart2 with clock mehanics
   SBT.begin("ClockStation"); //BT Name
   
+  WiFi.begin(ssid, Wpass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+  }
+  WiFi.disconnect(true);
+
+  server.on("/serverIndex", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", UpdatePage);
+  });
+
+  server.on("/update", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+        Update.printError(SBT);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      /* flashing firmware to ESP*/
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(SBT);
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) { //true to set the size to the current progress
+        
+      } else {
+        Update.printError(SBT);
+      }
+    }
+  });
+  server.begin();
 }
 
 ///repo test
 
 
 void loop() {
-
+  // server.handleClient();
   
   SBT.print("program start\r\n");
+  SBT.println(WiFi.localIP());
 
   unsigned char *  command = buildCMD(ReadSerialNCMD, ParametrsCMD);
 
@@ -168,7 +247,7 @@ void loop() {
   SBT.println("");
   SBT.println(" ^^^ CM ^^^");
   free(cmanswer);
-  delay(5000);
+  delay(1000);
 
 SBT.flush();
 CMSerial.flush();
